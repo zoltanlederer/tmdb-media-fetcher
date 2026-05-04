b27274d440457b3ab56ba6db5f93197b441d9595
Zoltan Lederer <23390588+zoltanlederer@users.noreply.github.com>
2026-05-04 21:48:32 +0200

    Add config example file

---
+++ config.example.py
@@ -0,0 +1,7 @@
+# Database
+DB_PATH = "/absolute/path/to/your/media.db"
+
+# TMDB API
+TMDB_API_KEY = "your_tmdb_api_key_here"
+TMDB_BASE_URL = "https://api.themoviedb.org/3"
+TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500"
